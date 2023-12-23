fa9564c5c66e453762d40476bc309c60f5289ed8
Mykola <myksolarik@gmail.com>
2023-12-23 12:43:07 +0000

    Improve PoolAllocator speed by introducing an aray of free chunks

--- src/PoolAllocator.h
+++ src/PoolAllocator.h
@@ -12,14 +12,15 @@ namespace MemAlloc
 
 		PoolAllocator(const PoolAllocator& poolAllocator) : AllocatorInterface(poolAllocator)
 		{
+			m_start_ptr = poolAllocator.m_start_ptr;
+			m_freeChunks = poolAllocator.m_freeChunks;
 			m_chunkSize = poolAllocator.m_chunkSize;
 			m_chunksNum = poolAllocator.m_chunksNum;
-			m_freeList = poolAllocator.m_freeList;
-			m_start_ptr = poolAllocator.m_start_ptr;
+			m_currFreeChunksIdx = poolAllocator.m_currFreeChunksIdx;
 		}
 
 		PoolAllocator(const std::size_t chunksNum, const std::size_t chunkSize)
-			: AllocatorInterface(chunksNum*chunkSize), m_chunksNum(chunksNum), m_chunkSize(chunkSize)
+			: AllocatorInterface(chunksNum*chunkSize), m_chunksNum(chunksNum), m_chunkSize(chunkSize), m_currFreeChunksIdx(m_chunksNum -1)
 		{
 			assert(((chunkSize % sizeof(std::size_t))==0) && "Chunk size must be aligned to std::size_t");
 			assert(m_totalSize % chunkSize == 0 && "Total Size must be a multiple of Chunk Size");
@@ -28,29 +29,34 @@ namespace MemAlloc
 		void Init() override
 		{
 			m_start_ptr = malloc(m_totalSize);
+			m_freeChunks = (void**)malloc(m_chunksNum * sizeof(void*));
+
 			Reset();
 		}
 
 		~PoolAllocator() override
 		{
 			free(m_start_ptr);
+			free(m_freeChunks);
 		}
 
-		void* Allocate(const std::size_t allocationSize, const std::size_t alignment = 8) override
+		void* Allocate(const std::size_t allocationSize, const std::size_t alignment = sizeof(std::size_t)) override
 		{
 			assert(allocationSize <= m_chunkSize && "Allocation size must be <= to chunk size");
 
 			SpinlockGuard guard(m_spinlock);
 
-			Node* freeNode = m_freeList.pop();
-
-			assert(freeNode != nullptr && "The pool allocator is full");
-			if (freeNode)
+			assert(m_currFreeChunksIdx >= 0 && "The pool allocator is full");			
+			if (m_currFreeChunksIdx < 0)
 			{
-				m_used += m_chunkSize;
+				return nullptr;
 			}
 
-			return (void*)freeNode;
+			void* freeChunk = m_freeChunks[m_currFreeChunksIdx--];
+			
+			m_used += m_chunkSize;
+
+			return freeChunk;
 		}
 
 		bool Free(void* ptr) override
@@ -65,7 +71,7 @@ namespace MemAlloc
 
 			m_used -= m_chunkSize;
 
-			m_freeList.push(static_cast<Node*>(ptr));
+			m_freeChunks[++m_currFreeChunksIdx] = ptr;
 
 			return true;
 		}
@@ -75,15 +81,14 @@ namespace MemAlloc
 			SpinlockGuard guard(m_spinlock);
 
 			m_used = 0;
-			// Create a linked-list with all free positions
-			const std::size_t nChunks = m_totalSize / m_chunkSize;
-			for (std::size_t i = 0; i < nChunks; ++i)
-			{
-				const std::size_t address = reinterpret_cast<std::size_t>(m_start_ptr) + static_cast<
-					std::size_t>(i) * m_chunkSize;
 
-				m_freeList.push(reinterpret_cast<Node*>(address));
+			for (std::size_t i = 0; i < m_chunksNum; ++i)
+			{
+				const std::size_t address = reinterpret_cast<std::size_t>(m_start_ptr) + (i) * m_chunkSize;
+				m_freeChunks[i] = reinterpret_cast<void*>(address);
 			}
+
+			m_currFreeChunksIdx = m_chunksNum - 1;
 		}
 
 		std::size_t GetChunkSize() const
@@ -92,13 +97,9 @@ namespace MemAlloc
 		}
 
 	private:
-		struct FreeHeader
-		{
-		};
-
-		using Node = StackLinkedList<FreeHeader>::Node;
-		StackLinkedList<FreeHeader> m_freeList;
+		void** m_freeChunks = nullptr;
 		void* m_start_ptr = nullptr;
+		int64_t m_currFreeChunksIdx = -1;
 		std::size_t m_chunkSize = 0;
 		std::size_t m_chunksNum = 0;
 		Spinlock m_spinlock;
